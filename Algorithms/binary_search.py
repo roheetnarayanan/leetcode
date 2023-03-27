500716317e2540d4628fbbc668d07ad60bad999e
Roheet <roheet.n@gmail.com>
2023-03-27 16:37:30 +0200

    Create binary_search.py

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        left, right = 0, len(nums)-1
+        while left < right:
+            middle = (left+right)//2
+
+            if nums[middle] == target:
+                return middle
+
+            if target > nums[middle]:
+                left = middle+1
+
+            if target < nums[middle]:
+                right = middle-1
+        return -1
+
+ 
